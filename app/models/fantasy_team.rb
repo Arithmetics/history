ef0a7229cc12648d4081f1b4f13979847bcf21fb
Arithmetics <brock.m.tillotson@gmail.com>
2019-09-28 17:34:42 -0700

    finished fantasy team tests and added basic player model

--- app/models/fantasy_team.rb
+++ app/models/fantasy_team.rb
@@ -1,3 +1,5 @@
 class FantasyTeam < ApplicationRecord
   belongs_to :owner
+  validates :name, presence: true
+  validates :year, presence: true
 end
--- test/models/fantasy_team_test.rb
+++ test/models/fantasy_team_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class FantasyTeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @valid_team = fantasy_teams(:squad_one)
+  end
 
+  test "valid fantasy team" do
+    assert @valid_team.valid?
+  end
+
+  test "invalid without name" do
+    invalid_team = FantasyTeam.new(year: 2019)
+    refute invalid_team.valid?, "fantasy team not valid without a name"
+  end
+
+  test "invalid without year" do
+    invalid_team = FantasyTeam.new(name: "asdf")
+    refute invalid_team.valid?, "fantasy team not valid without a year"
+  end
 end
