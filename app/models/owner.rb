1f33791a8141b98ccdfcca14bf52bee5bb0e85b1
Arithmetics <brock.m.tillotson@gmail.com>
2019-10-28 21:12:49 -0700

    added playoff game cuum stats

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,9 +5,8 @@ class Owner < ApplicationRecord
   has_many :fantasy_teams
   has_many :away_fantasy_games, through: :fantasy_teams
   has_many :home_fantasy_games, through: :fantasy_teams
-  
-  has_many :purchases, through: :fantasy_teams
 
+  has_many :purchases, through: :fantasy_teams
 
   def cumulative_stats
     total_points = 0
@@ -34,6 +33,30 @@ class Owner < ApplicationRecord
 
     points_per_game = (total_points / total_games).round(2)
 
-    return { "total_points" => total_points.round(2), "total_games" => total_games, "total_wins" => total_wins, "points_per_game" => points_per_game }
+    total_playoff_points = 0
+    total_playoff_games = 0
+    total_playoff_wins = 0
+    @away_playoff_games = self.away_fantasy_games.where(week: 14..16)
+    @home_fantasy_games = self.home_fantasy_games.where(week: 14..16)
+
+    @away_playoff_games.each do |game|
+      total_playoff_games += 1
+      total_playoff_points += game.away_score
+      if game.away_score > game.home_score
+        total_playoff_wins += 1
+      end
+    end
+
+    @home_fantasy_games.each do |game|
+      total_playoff_games += 1
+      total_playoff_points += game.home_score
+      if game.home_score > game.away_score
+        total_playoff_wins += 1
+      end
+    end
+
+    playoff_points_per_game = total_playoff_games > 0 ? (total_playoff_points / total_playoff_games).round(2) : 0
+
+    return { "total_points" => total_points.round(2), "total_games" => total_games, "total_wins" => total_wins, "points_per_game" => points_per_game, "total_playoff_points" => total_playoff_points.round(2), "total_playoff_games" => total_playoff_games, "total_playoff_wins" => total_playoff_wins, "playoff_points_per_game" => playoff_points_per_game }
   end
 end
