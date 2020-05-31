0238eeff5da565b6ba1cea6cbcfa5d3f9d3697a3
Arithmetics <brock.m.tillotson@gmail.com>
2020-05-31 12:16:24 -0700

    fixed error

--- db/migrate/20200531183822_add_default_nfl_urls.rb
+++ db/migrate/20200531183822_add_default_nfl_urls.rb
@@ -4,11 +4,14 @@ class AddDefaultNflUrls < ActiveRecord::Migration[6.0]
       player.nfl_URL_name = player.name.gsub(" ", "-").gsub(".", "-").gsub("--", "-").downcase
       player.save!
     end
-    x = Player.find(2507175).nfl_URL_name = "steve-smith-2"
+    x = Player.find(2507175)
+    x.nfl_URL_name = "steve-smith-2"
     x.save!
-    y = Player.find(2508048).nfl_URL_name = "mike-williams-4"
+    y = Player.find(2508048)
+    y.nfl_URL_name = "mike-williams-4"
     y.save!
-    z = Player.find(2506140).nfl_URL_name = "ryan-grant-2"
+    z = Player.find(2506140)
+    z.nfl_URL_name = "ryan-grant-2"
     z.save!
   end
 
