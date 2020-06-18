f9f392ca02f722df68246f13bea33e93c452f3e4
Arithmetics <brock.m.tillotson@gmail.com>
2020-06-17 19:06:37 -0700

    query update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
 
     @owners = Owner.all.includes(fantasy_teams: { away_fantasy_games: { away_fantasy_team: :owner, home_fantasy_team: :owner }, home_fantasy_games: { away_fantasy_team: :owner, home_fantasy_team: :owner } }, away_fantasy_games: {}, home_fantasy_games: {})
 
-    @first_starts = FantasyStart.includes(:player).where(week: 13, year: 2019).where.not(position: "BN").where(player_id: FantasyStart.select(:player_id).where.not(position: "BN").group(:player_id).having("count(*) = 1").pluck(:player_id))
+    @first_starts = FantasyStart.includes(:player).where(week: 4, year: 2019).where.not(position: "BN").where(player_id: FantasyStart.where.not(position: "BN").group(:player_id).having("count(*) = 1").select(:player_id))
   end
 end
