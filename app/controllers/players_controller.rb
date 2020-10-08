7fb2569d0b2dbb3efc0d7a1ca3370964bb4cacee
Arithmetics <brock.m.tillotson@gmail.com>
2020-10-07 18:36:51 -0700

    added protected delete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :authenticate_admin!, only: [:create, :update]
+  before_action :authenticate_admin!, only: [:create, :update, :delete]
 
   def index
     @players = Player.includes(fantasy_starts: { fantasy_team: :owner }, purchases: { fantasy_team: :owner }, season_stats: {}, rankings: {}).all
