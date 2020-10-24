90f9491cbc449c4f99be9b36203e3ca4844b42dd
Arithmetics <brock.m.tillotson@gmail.com>
2020-10-24 10:20:32 -0700

    added a debugger and removed the blanket cors config for now as it seem sunneeded

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,13 +14,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-  allow do
-    origins "*"
-    resource(
-      "*",
-      headers: :any,
-      expose: ["Authorization"],
-      methods: [:get, :patch, :put, :delete, :post, :options, :show],
-    )
-  end
+  # allow do
+  #   origins "*"
+  #   resource(
+  #     "*",
+  #     headers: :any,
+  #     expose: ["Authorization"],
+  #     methods: [:get, :patch, :put, :delete, :post, :options, :show],
+  #   )
+  # end
 end
