68ef0d2868b123dacd7afb467ed1128e41c7a0a9
Arithmetics <brock.m.tillotson@gmail.com>
2020-11-23 20:16:49 -0800

    more heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,27 +1,27 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins "http://localhost:3000"
-  #   resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  # end
+  allow do
+    origins "http://localhost:3000"
+    resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-  # allow do
-  #   origins "https://the97062lab.herokuapp.com"
-  #   resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  # end
+  allow do
+    origins "herokuapp.com"
+    resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
   # allow do
   #   origins "http://the97062lab.herokuapp.com"
   #   resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   # end
 
-  allow do
-    origins "*"
-    resource(
-      "*",
-      headers: :any,
-      expose: ["Authorization"],
-      methods: [:get, :patch, :put, :delete, :post, :options, :show],
-      credentials: true,
-    )
-  end
+  # allow do
+  #   origins "*"
+  #   resource(
+  #     "*",
+  #     headers: :any,
+  #     expose: ["Authorization"],
+  #     methods: [:get, :patch, :put, :delete, :post, :options, :show],
+  #     credentials: true,
+  #   )
+  # end
 end
