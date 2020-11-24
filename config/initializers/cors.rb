26ef10fe4f9ddf1b7d583e4f3f99c41a44312b33
Arithmetics <brock.m.tillotson@gmail.com>
2020-11-23 20:23:13 -0800

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "the97062lab.herokuapp.com"
+    origins "https://the97062lab.herokuapp.com"
     resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
