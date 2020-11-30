06141887a7203fdc26a7f87a82f08d80491c7e21
Brock Tillotson <brock.m.tillotson@gmail.com>
2020-11-29 20:05:46 -0800

    new backup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-  # allow do
-  #   origins "http://the97062lab.herokuapp.com"
-  #   resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  # end
+   allow do
+     origins "http://the97062lab.herokuapp.com"
+     resource "*", headers: :any, expose: ["Authorization"], methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
   # allow do
   #   origins "*"
