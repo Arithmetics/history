fbe4cb4ff2cc6e3bca3aa2e98fdecff7d5d69add
Arithmetics <brock.m.tillotson@gmail.com>
2021-02-21 20:23:55 -0800

    effects

--- app/models/season_card_effect.rb
+++ app/models/season_card_effect.rb
@@ -1,3 +1,9 @@
 class SeasonCardEffect < ApplicationRecord
   has_many :season_card_ownerships
+
+  self.get_random_effect
+    count = SeasonCardEffect.count
+    random_offset = rand(count)
+    random_user = SeasonCardEffect.offset(random_offset).first
+  end
 end
